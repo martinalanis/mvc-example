cb5d62b620250e2cfb9721e28ecd6515ebff6487
martinalanis <martinalanis.dev@gmail.com>
2021-02-13 00:06:54 -0600

    FIX: bug de archivo config

--- core/app.php
+++ core/app.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'config/config.php';
 require_once 'controller.php';
 require_once 'controllers/error.php';
 require_once 'view.php';
--- views/main/index.php
+++ views/main/index.php
@@ -2,9 +2,4 @@
 	<h2 class="mb-5">
 		Bienvenido
 	</h2>
-	<div class="row">
-		<div class="col">
-			<a href="agregar" class="btn btn-success">agregar producto</a>
-		</div>
-	</div>
 </div>
